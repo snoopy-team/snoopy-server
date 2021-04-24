fd59b43193fc5d1e34d26f8aef8228e4ae739aaf
qinjoshua <qinjoshuac@outlook.com>
2021-04-24 00:16:59 -0400

    Merge Conflicts resolution

--- src/main/java/com/server/Controller/GameController.java
+++ src/main/java/com/server/Controller/GameController.java
@@ -32,18 +32,16 @@ public class GameController {
     return game.step(1, actions.getActions());
   }
 
-//  @MessageMapping("/gameconfig-to-server")
-//  @SendTo("/game/gameconfig-to-client")
-//  public GameStateJSON gameconfig() throws Exception {
-//    if (game == null) {
-//      Player player = new Player(Constants.STARTING_POSITION, Constants.STARTING_ORIENTATION, ids);
-//      ids++;
-//      game = new Game(player);
-//    }
-//
-//    //  This is hardcoded as 1 for convenience. If we're still pretending that we'll one day
-//    //  support multiplayer really this whole controller needs to be thoroughly refactored, but
-//    //  this especially.
-//    return null; //game.step(1, actions.getActions());
-//  }
+  @MessageMapping("/config-to-server")
+  @SendTo("/game/config-to-client")
+  public GameStateJSON gameconfig() throws Exception {
+    if (game == null) {
+      game = new Game();
+    }
+
+    //  This is hardcoded as 1 for convenience. If we're still pretending that we'll one day
+    //  support multiplayer really this whole controller needs to be thoroughly refactored, but
+    //  this especially.
+    return null; //game.step(1, actions.getActions());
+  }
 }
